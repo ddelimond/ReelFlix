360284522af82a6c11a56ed5d1ec10a47ecd4600
Darren Delimond <darrendelimond@gmail.com>
2022-10-27 16:55:24 -0400

    =added header and banner

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // middleware
 app.set('view engine', 'ejs');
-app.get(express.static('public'));
+app.use(express.static('public'));
 app.get(express.urlencoded({ extended: true }));
 
 
@@ -25,86 +25,121 @@ mongoose.connect(
 );
 
 
-
-const server = http.createServer((req, res) => {
-    let file = path.basename(req.url);
-    let extension = path.extname(req.url);
-    let contentType;
-
-    if (req.url === '/') {
-        file = 'index.html';
-        filePath = path.join(__dirname, 'public', file)
-        contentType = 'text/html';
-        extension = '.html';
-    } else {
-
-        switch (extension) {
-            case '.css':
-                contentType = 'text/css';
-                filePath = path.join(__dirname, 'public', 'Css', file)
-                break;
-            case '.jpeg':
-                contentType = 'image/jpeg';
-                filePath = path.join(__dirname, 'public', 'Imgs', file)
-                break;
-            case '.jpg':
-                contentType = 'image/jpg';
-                filePath = path.join(__dirname, 'public', 'Imgs', file)
-                break;
-            case '.png':
-                contentType = 'image/png';
-                filePath = path.join(__dirname, 'public', 'Imgs', file)
-                break;
-            case '.js':
-                contentType = 'text/javascript';
-                filePath = path.join(__dirname, 'public', 'Js', file)
-                break;
-            case '.json':
-                contentType = 'application/json';
-                break;
-            case '.webp':
-                contentType = 'image/webp';
-                filePath = path.join(__dirname, 'public', 'Imgs', file)
-                break;
-            case '.webmanifest':
-                contentType = 'application/manifest+json';
-                filePath = path.join(__dirname, 'public', 'fav', file)
-                break;
-            case '.ico':
-                contentType = 'image/vnd.microsoft.icon';
-                filePath = path.join(__dirname, 'public', 'fav', file)
-                break;
-
-            case '.mp4':
-                contentType = 'video/mp4';
-                break;
-            case '.mpeg':
-                contentType = 'video/mpeg';
-                break;
-        }
-    }
-
-
-    console.log(req.url)
-    // console.log(file)
-    console.log(contentType)
-
-    res.writeHead(200, { 'Content-Type': contentType });
-    fs.readFile(filePath, (err, file) => {
-        if (err) {
-            throw err
-        } else {
-            res.end(file)
-        }
-    })
-});
-
-
-server.listen(port, () => {
-    console.log('Server is running better catch it...')
+app.listen(port, () => {
+    console.log('Server is running go catch it ...')
 })
 
 
-// const server = app.listen(port, () => {
-//     console.log('Server is running go catch it ...')
-// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const server = http.createServer((req, res) => {
+//     let file = path.basename(req.url);
+//     let extension = path.extname(req.url);
+//     let contentType;
+
+//     if (req.url === '/') {
+//         file = 'index.html';
+//         filePath = path.join(__dirname, 'public', file)
+//         contentType = 'text/html';
+//         extension = '.html';
+//     } else {
+
+//         switch (extension) {
+//             case '.css':
+//                 contentType = 'text/css';
+//                 filePath = path.join(__dirname, 'public', 'Css', file)
+//                 break;
+//             case '.jpeg':
+//                 contentType = 'image/jpeg';
+//                 filePath = path.join(__dirname, 'public', 'Imgs', file)
+//                 break;
+//             case '.jpg':
+//                 contentType = 'image/jpg';
+//                 filePath = path.join(__dirname, 'public', 'Imgs', file)
+//                 break;
+//             case '.png':
+//                 contentType = 'image/png';
+//                 filePath = path.join(__dirname, 'public', 'Imgs', file)
+//                 break;
+//             case '.js':
+//                 contentType = 'text/javascript';
+//                 filePath = path.join(__dirname, 'public', 'Js', file)
+//                 break;
+//             case '.json':
+//                 contentType = 'application/json';
+//                 break;
+//             case '.webp':
+//                 contentType = 'image/webp';
+//                 filePath = path.join(__dirname, 'public', 'Imgs', file)
+//                 break;
+//             case '.webmanifest':
+//                 contentType = 'application/manifest+json';
+//                 filePath = path.join(__dirname, 'public', 'fav', file)
+//                 break;
+//             case '.ico':
+//                 contentType = 'image/vnd.microsoft.icon';
+//                 filePath = path.join(__dirname, 'public', 'fav', file)
+//                 break;
+
+//             case '.mp4':
+//                 contentType = 'video/mp4';
+//                 break;
+//             case '.mpeg':
+//                 contentType = 'video/mpeg';
+//                 break;
+//         }
+//     }
+
+
+//     console.log(req.url)
+//     // console.log(file)
+//     console.log(contentType)
+
+//     res.writeHead(200, { 'Content-Type': contentType });
+//     fs.readFile(filePath, (err, file) => {
+//         if (err) {
+//             throw err
+//         } else {
+//             res.end(file)
+//         }
+//     })
+// });
